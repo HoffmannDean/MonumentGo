f5635bd67ea2c46a6fadc5106f7d2ef22f63de33
AlbrErik <erik.albrecht@stud.uni-hannover.de>
2025-12-17 17:51:02 +0100

    Refactoring

---
+++ app/src/main/java/de/luh/hci/mid/monumentgo/map/domain/AppNavigation.kt
@@ -0,0 +1,36 @@
+package de.luh.hci.mid.monumentgo.map.domain
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import de.luh.hci.mid.monumentgo.map.ui.MainMapScreen
+import de.luh.hci.mid.monumentgo.map.domain.Screen
+import de.luh.hci.mid.monumentgo.ui.screens.ProfileScreen
+import de.luh.hci.mid.monumentgo.ui.screens.SettingsScreen
+import de.luh.hci.mid.monumentgo.ui.screens.StatsScreen
+
+@Composable
+fun AppNavigation(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Screen.MainMap.route) {
+
+        composable(Screen.MainMap.route) {
+            MainMapScreen(navController)
+        }
+
+        composable(Screen.Stats.route) {
+            StatsScreen(navController)
+        }
+
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController)
+        }
+
+        composable(Screen.Settings.route) {
+            SettingsScreen(navController)
+        }
+
+    }
+}
